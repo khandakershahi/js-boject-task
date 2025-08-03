9584d544deda28eb31bdef229eef1259e75d6229
khandakershahi <khandakershahi@gmail.com>
2025-08-03 17:57:40 +0600

    task-5 done

---
+++ task-5.js
@@ -0,0 +1,37 @@
+/**
+ * Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+Output:
+
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+ */
+
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+    };
+
+    for(const prop in myObject){
+        console.log('key: ' + prop + ' | ' + 'type: ' + typeof(myObject[prop])) ;
+    } 
+
+
+
+  
